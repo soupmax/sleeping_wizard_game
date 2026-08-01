c568e292b8362abe20f37914a021e9df8e332e74
Schnabelfachkraft <Patrick.Pelc03@gmail.com>
2026-08-01 19:31:51 +0200

    Menu erstellt und verbunden mit scenen

---
+++ scripts/main_menu.gd
@@ -0,0 +1,13 @@
+extends Node
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
